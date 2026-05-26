1ae9f12c37059122677250f3cc97b70c03efc41a
BizzOne-Digital <Development@bizzonedigital.com>
2026-05-26 09:01:05 -0700

    serverjss

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
+const careersRouter = require('./routes/careers');
+app.use('/api', careersRouter);
 dotenv.config();
 
 const app = express();
